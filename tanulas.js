e922138b44dadadb9683b64ab71b03dd88604c8f
Aur316 <kristianfeher86@gmail.com>
2024-03-08 12:40:13 +0100

    ciklusok

---
+++ tanulas.js
@@ -0,0 +1,54 @@
+// var krisz = "audi";
+// console.log(krisz, "ezt irjuk ki");
+// const szam = 22;
+// //konstal irunk valamit az nem valtoztathato
+// console.log(szam, "szam");
+// let a = true;
+// //let valtoztathato barmire
+// console.log(a, "true");
+// a = false;
+// console.log(a, "megvaltoztattuk az igazat");
+
+// if (szam > 10) {
+//   console.log("szam nagyobb mint 10");
+// } else {
+//   console.log("szam kisebb mint 10");
+// }
+// for (i = 1; i <= 10; i++) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+
+// for (i = 30; i <= 100; i += 7) {
+//   console.log(i);
+// }
+// for (i = 112; i >= 2; i -= 11) {
+//   console.log(i);
+// }
+// for (i = -90; i <= 90; i += 15) {
+//   console.log(i);
+// }
+// for (i = 1; i < 18; i++) {
+//   for (o = 1; o < 18; o++) {
+//     if (i + o === 18) {
+//       console.log(i, o);
+//     }
+//   }
+// }
+// for (i = 1; i <= 10; i++) {
+//   const a = 5;
+//   let b = i * a;
+//   console.log(i, "*", a, "=", b);
+// }
+// for (i = 1; i <= 15; i++) {
+//   let b = i * i;
+//   console.log(b);
+// } i
+// for (i = 1; i <= 144; i++) {
+//   if (144 % i === 0) console.log(i);
+// }
+for (i = 0; i <= 10; i++) {
+  let b = 2 ** i;
+  console.log(b);
+}
